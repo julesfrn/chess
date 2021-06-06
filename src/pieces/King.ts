a0ca9b270a477443da4976b3c2c566cd37f14296
Jules Fournier <jules.fournier.1210@gmail.com>
2021-06-06 12:58:43 +0200

    add king basic move validation

--- src/pieces/King.ts
+++ src/pieces/King.ts
@@ -23,7 +23,8 @@ export default class King extends Piece {
       : 'https://images.chesscomfiles.com/chess-themes/pieces/tournament/150/bk.png'
   }
 
-  public isAllowedToMoveTo(currentSquare: ISquare, newSquare: ISquare, board: Board): boolean {
+  public isAllowedToMoveTo(_currentSquare: ISquare, newSquare: ISquare, _board: Board): boolean {
+    if (newSquare.piece?.color === this.color) return false
     return true
   }
 }
